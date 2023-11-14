95b22ce0e611d224a36c28c740022f76e12a6d86
dinhminhthong <dinhminhthong14022001@gmail.com>
2023-11-14 16:13:48 +0700

    giai_thuat_ngay_1

--- giai_thuat/DemSoLanSuatHien.java
+++ giai_thuat/DemSoLanSuatHien.java
@@ -1,2 +1,23 @@
 public class DemSoLanSuatHien {
+    static void demKyTu(String str) {
+        int[] counter = new int[256];
+        for (int i = 0; i < str.length(); i++)
+            counter[str.charAt(i)]++;
+        char[] array = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            array[i] = str.charAt(i);
+            int flag = 0;
+            for (int j = 0; j <= i; j++) {
+                if (str.charAt(i) == array[j])
+                    flag++;
+            }
+            if (flag == 1)
+                System.out.println("Số lần xuất hiện của " + str.charAt(i) + " trong chuỗi:" + counter[str.charAt(i)]);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "aabacsdc";
+        System.out.println("Chuỗi là: " + str);
+        demKyTu(str);
+    }
 }
---
+++ giai_thuat/PhuongTrinhBac1.java
@@ -0,0 +1,2 @@
+public class PhuongTrinhBac1 {
+}
